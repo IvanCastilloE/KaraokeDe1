21999a67893c3bfb49c22cbc706c6b429b753e7c
Angel Ivan Castillo <angel_xayan06@hotmail.com>
2020-01-30 16:22:41 -0700

    Con eso tiene

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -65,7 +65,7 @@ namespace Reproductor
                 lbl1.Text = "Every corner of her heart was filled with her love for him";
             if (reader.CurrentTime.TotalSeconds >=21  && reader.CurrentTime.TotalSeconds < 24)
                 lbl1.Text = "Even when the time would come to say goodbye";
-            if (reader.CurrentTime.TotalSeconds >= 24 && reader.CurrentTime.TotalSeconds < 27)
+            if (reader.CurrentTime.TotalSeconds > 24 && reader.CurrentTime.TotalSeconds < 27)
                 lbl1.Text = "This is what she'd say:";
             if (reader.CurrentTime.TotalSeconds >= 27 && reader.CurrentTime.TotalSeconds <30)//Dos veces
                 lbl1.Text = "There's nothing to cry for I want you to know that";
@@ -83,6 +83,8 @@ namespace Reproductor
                 lbl1.Text = "The only thing unreal was";
             if (reader.CurrentTime.TotalSeconds >= 50 && reader.CurrentTime.TotalSeconds <54)
                 lbl1.Text = "my place in your heart";
+            if (reader.CurrentTime.TotalSeconds >54 && reader.CurrentTime.TotalSeconds < 64)
+                lbl1.Text = " ";
 
             if (reader.CurrentTime.TotalSeconds >= 64 && reader.CurrentTime.TotalSeconds <67)
                 lbl1.Text = "She could feel his words wrap around her neck";
@@ -105,31 +107,57 @@ namespace Reproductor
             if (reader.CurrentTime.TotalSeconds >=93 && reader.CurrentTime.TotalSeconds < 96)
                 lbl1.Text = "They'll never bring back all the things that they took";
             //aqui me quede
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >= 96 && reader.CurrentTime.TotalSeconds < 100)
                 lbl1.Text = "So I won't turn back, I'll find you again someday";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
-                lbl1.Text = "I know";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >= 100 && reader.CurrentTime.TotalSeconds <103)
+                lbl1.Text = "I know oh";
+            if (reader.CurrentTime.TotalSeconds >= 103 && reader.CurrentTime.TotalSeconds < 106)
                 lbl1.Text = "If only the tears would erase all the memories";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >= 106 && reader.CurrentTime.TotalSeconds < 109)
                 lbl1.Text = "Then maybe I'd learn to fight all these fears";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >= 109 && reader.CurrentTime.TotalSeconds < 113)
                 lbl1.Text = "Won't hesitate, won't run away";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >=113 && reader.CurrentTime.TotalSeconds < 116)
                 lbl1.Text = "These are things I'm scared to say...";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >=116 && reader.CurrentTime.TotalSeconds < 143)
+                lbl1.Text = " ";
+
+            if (reader.CurrentTime.TotalSeconds >= 143  && reader.CurrentTime.TotalSeconds < 146)
                 lbl1.Text = "As he is standing in light that is fading";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >=146  && reader.CurrentTime.TotalSeconds < 149)
                 lbl1.Text = "He's feeling the trembling beat of his heart";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >= 149 && reader.CurrentTime.TotalSeconds < 153)
                 lbl1.Text = "And he remembers the sound of her very last words";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >=153 && reader.CurrentTime.TotalSeconds < 155)
                 lbl1.Text = "Again";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds > 153 && reader.CurrentTime.TotalSeconds < 155)
+                lbl1.Text = " ";
+            if (reader.CurrentTime.TotalSeconds > 155 && reader.CurrentTime.TotalSeconds < 156)
+                lbl1.Text = "Again";
+            if (reader.CurrentTime.TotalSeconds >= 156 && reader.CurrentTime.TotalSeconds < 159)//Dos veces
+                lbl1.Text = "There's nothing to cry for I want you to know that";
+            if (reader.CurrentTime.TotalSeconds >=159 && reader.CurrentTime.TotalSeconds < 162)//dos  veces
+                lbl1.Text = "As long as it's you, this pain couldn't hurt me";
+            if (reader.CurrentTime.TotalSeconds >= 162 && reader.CurrentTime.TotalSeconds < 166)//dos veces
+                lbl1.Text = "No reason to regret, I'll always be waiting for you";
+            if (reader.CurrentTime.TotalSeconds >= 166 && reader.CurrentTime.TotalSeconds < 168)//tres veces
+                lbl1.Text = "My love";
+            if (reader.CurrentTime.TotalSeconds >168 && reader.CurrentTime.TotalSeconds < 169)//tres veces
+                lbl1.Text = "My love";
+            if (reader.CurrentTime.TotalSeconds >= 169 && reader.CurrentTime.TotalSeconds < 172)//dos veces
+                lbl1.Text = "It'll be alright I want you to know that";
+            if (reader.CurrentTime.TotalSeconds >= 172 && reader.CurrentTime.TotalSeconds < 175)//dos veces
+                lbl1.Text = "I'll always believe in the truth of your words";
+
+            if (reader.CurrentTime.TotalSeconds >=175 && reader.CurrentTime.TotalSeconds < 179)
                 lbl1.Text = "With open arms, he looks to the sky";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >=179 && reader.CurrentTime.TotalSeconds < 183)
                 lbl1.Text = "Finally, I've found my way";
-            if (reader.CurrentTime.TotalSeconds > 4 && reader.CurrentTime.TotalSeconds <= 6)
+            if (reader.CurrentTime.TotalSeconds >=183 && reader.CurrentTime.TotalSeconds <= 187)
+                lbl1.Text = "My way to you";
+            if (reader.CurrentTime.TotalSeconds > 187 && reader.CurrentTime.TotalSeconds < 189)
+                lbl1.Text = " ";
+            if (reader.CurrentTime.TotalSeconds >=189  && reader.CurrentTime.TotalSeconds <= 194)
                 lbl1.Text = "My way to you";
 
         }
